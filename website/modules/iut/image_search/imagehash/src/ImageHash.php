e60f0b226d5cbf63c10c2c019a0bdc6bb997d363
HRezaei <mr.hrezaei@gmail.com>
2016-12-21 15:58:38 +0330

    Added some new image hashing methods

--- website/modules/iut/image_search/imagehash/src/ImageHash.php
+++ website/modules/iut/image_search/imagehash/src/ImageHash.php
@@ -1,17 +1,36 @@
 <?php
 
 
-namespace Drupal\bhash;
+namespace Drupal\imagehash;
 
 
-class BlockHash {
+class ImageHash {
 	
+	public static $default_method = 'whash'; 
 	
-	public static function generate($uri, $bits = 8) {
+	public static function bhash($uri, $bits = 8) {
 		$command = "blockhash -b $bits $uri";
 	
 		$last_line = exec($command, $output, $return_var);
 		$hash = substr($last_line, 0, pow($bits, 2)/4);
+		return $hash;
+	}
+	
+	public static function phash($uri, $bits = 8) {
+		$module_path = drupal_get_path('module', 'imagehash');
+		$path = realpath($module_path . '/scripts/phash.py');
+		$command = "python $path $uri $bits";
+	
+		$hash = exec($command, $output, $return_var);
+		return $hash;
+	}
+	
+	public static function whash($uri, $bits = 8) {
+		$module_path = drupal_get_path('module', 'imagehash');
+		$path = realpath($module_path . '/scripts/whash.py');
+		$command = "python $path $uri $bits";
+	
+		$hash = exec($command, $output, $return_var);
 		return $hash;
 	}
 }
